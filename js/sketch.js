075a285e970a53f2d1185c3dd68b2605bb552d6c
Arata <mau8fg@gmail.com>
2021-01-04 18:33:33 +0900

    add bloom

--- js/sketch.js
+++ js/sketch.js
@@ -2,12 +2,18 @@
 var angle = 0;
 
 var slider;
+let button;
+
+let red = 0;
+let green = 0;
+let blue = 0;
+let size = 1;
 
 function setup(){
    // 描画領域のセットアップ
   createCanvas(400,400);
   slider = createSlider(0, TWO_PI, PI/6, 0.01);
-
+  button = createButton("bloom");
 }
 
 function draw(){
@@ -17,8 +23,21 @@ function draw(){
   
   translate(200, height); //座標軸の移動
   
-  stroke(0,200,200,200);
+  stroke(0, 255, 255);
   branch(100);
+
+
+  button.mousePressed(bloom);
+}
+
+
+
+function bloom() {
+   red = random(255);
+   green = random(255);
+   blue = random(255);
+   size = random(1,10);
+  
 }
 
 function branch(len){
@@ -34,6 +53,11 @@ function branch(len){
     rotate(-angle);
     branch(len * 0.67);
     pop();
-  }  
+  } 
+  else{
+    noStroke();
+    fill(red,green,blue,150);
+    ellipse(0, 0, size, size);
+  } 
 }
 
